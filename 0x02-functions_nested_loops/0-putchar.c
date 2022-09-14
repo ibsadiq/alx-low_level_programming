6947257ea6b3195ffbc981e8bd3aa5b40aefb494
Abubakar Ibrahim <ibsadiq@live.com>
2022-09-14 10:37:44 -0700

    c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,6 @@
-#include "main.h"
-<<<<<<< HEAD
+ #include "main.h"
 /**
  * main - prints the word _putchar
-=======
-
-/**
- * main - prints the word _putchar
- *
->>>>>>> 2f681e23717a3d5e6f35e9ca4f654fb97ec09157
  * Return: 0 on success execution.
  */
 
@@ -15,22 +8,12 @@ int main(void)
 {
 	char s[] = "_putchar";
 	int i = 0;
-<<<<<<< HEAD
 	
-=======
-
->>>>>>> 2f681e23717a3d5e6f35e9ca4f654fb97ec09157
 	while (s[i] != '\0')
 	{
 		_putchar(s[i]);
 		i++;
 	}
 	_putchar(10);
-<<<<<<< HEAD
 	return (0);
 }
-=======
-
-	return (0);
-}
->>>>>>> 2f681e23717a3d5e6f35e9ca4f654fb97ec09157
