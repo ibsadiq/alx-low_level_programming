68dc3e2ad48b941698029f65b2f761ef46f9010b
Abubakar Ibrahim <ibsadiq@live.com>
2022-09-20 03:39:40 +0100

    pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,20 +1,24 @@
-#include <stdio.h>
-#include <string.h>
 #include "main.h"
 
 /**
  * print_rev - prints given string in reverse
+ *
  * @s: passed pointer argument for string
  * Return: void
  */
-
 void print_rev(char *s)
 {
-	int len = strlen(s);
+	int len = 0;
 
-    while (len--)
-        putchar(*(s + len));
-    putchar(10);
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+	len--;
+	while (len >= 0)
+	{
+		_putchar(*(s + len));
+		len--;
+	}
+	_putchar('\n');
 }
-
-
