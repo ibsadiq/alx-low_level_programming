cd5d564acc5698e58498f019dd45680ede425b72
Abubakar Ibrahim <ibsadiq@live.com>
2022-09-20 03:30:07 +0100

    pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,10 +10,9 @@
 
 void print_rev(char *s)
 {
-	int len = strlen(s);
-    
+	int len = strlen(s); 
     while (len--)
-            _putchar(*(s + len));
+        _putchar(*(s + len));
     _putchar(10);
 
 }
