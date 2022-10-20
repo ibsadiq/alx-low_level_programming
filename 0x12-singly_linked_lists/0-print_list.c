591ad8693dfdef4ea45d8255ed9e8fd34451375e
Abubakar Ibrahim <ibsadiq@live.com>
2022-10-20 19:38:38 +0100

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,21 +11,19 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t n_nodes;
+	size_t nbr_nodes=0;
 
-	if (!h)
-		return (0);
-	n_nodes = 0;
-	do {
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		h = h->next;
-		n_nodes++;
-	}  while (h);
-
-	return (n_nodes);
+	while (h != NULL)
+    {
+        if (h ->str == NULL)
+            printf("[%d] %s\n", 0,  "(nil)");
+        else
+            printf("[%d] %s\n", h->len, h->str);
+        
+        h = h->next;
+		nbr_nodes++;
+    }
+	
+	return (nbr_nodes);
 }
 
